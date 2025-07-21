2863b36e8240afdc7b1397171c0b882ce0b38db5
PraveenKumar-pk-star <praveenwork595@gmail.com>
2025-07-21 17:17:46 +0530

    Update openaiapp.py

--- openaiapp.py
+++ openaiapp.py
@@ -1,1 +1,15 @@
+! pip install openai
+from openai import OpenAI
+from getpass import getpass
+open_ai_key=getpass("Enter the keya:")
+client=OpenAI(api_key=open_ai_key)
+response=client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[{
+        "role":"user",
+        "content":"bed time story 2 lines"
+    }]
 
+)
+
+response.choices[0].message.content
